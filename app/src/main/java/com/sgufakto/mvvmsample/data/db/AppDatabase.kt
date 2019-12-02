f617a54c598135dd72f2ef871e79f2c87f7d04df
Donal Siagian <s.donal@indoliquid.id>
2019-12-02 10:33:37 +0700

    export schema database local is false

--- app/src/main/java/com/sgufakto/mvvmsample/data/db/AppDatabase.kt
+++ app/src/main/java/com/sgufakto/mvvmsample/data/db/AppDatabase.kt
@@ -8,7 +8,8 @@ import com.sgufakto.mvvmsample.data.db.entities.User
 
 @Database(
     entities = [User::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class AppDatabase: RoomDatabase() {
 
